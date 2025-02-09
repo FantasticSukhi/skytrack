98bd77bf45ef83b503ea93d73f67262a7780f23f
Blackmamba <122316561+FantasticSukhi@users.noreply.github.com>
2025-02-09 21:25:31 +0530

    Update airport_info.py

--- modules/airport_info.py
+++ modules/airport_info.py
@@ -44,7 +44,7 @@ def retrieve_value(line, val):
 def get_airport_info(airport_ident, airport_name): 
     headers = {
         'User-Agent': 'SKYTRACK: Aviation-based intelligence gathering tool'\
-        'Information at: https://github.com/ANG13T/skytrack'
+        'Information at: https://github.com/FantasticSukhi/skytrack'
     }
 
     if not os.path.exists(cache_path) or os.stat(cache_path).st_size == 0:
